08b5355286370ada03acfa46ed14ebc71b57ad59
maiple <maiple@gmail.com>
2020-05-24 11:24:58 -0700

    organized fn_todo list

---
+++ src/interpreter/library/todo/crypto.h
@@ -0,0 +1,19 @@
+/////////////// cryptography ////////////////
+
+// base64
+IGNORE_WARN(base64_decode)
+IGNORE_WARN(base64_encode)
+IGNORE_WARN(buffer_base64_decode)
+IGNORE_WARN(buffer_base64_decode_ext)
+IGNORE_WARN(buffer_base64_encode)
+
+// md5
+IGNORE_WARN(buffer_md5)
+IGNORE_WARN(md5_file)
+IGNORE_WARN(md5_string_unicode)
+
+// sha1
+IGNORE_WARN(buffer_sha1)
+IGNORE_WARN(sha1_file)
+IGNORE_WARN(sha1_string_unicode)
+IGNORE_WARN(sha1_string_utf8)
